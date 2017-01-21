6aa14f8afc56887085327a75c99906242054b44d
Jeroen Soeters <jsoeters@thoughtworks.com>
2017-01-20 19:26:04 -0500

    Removed ansible, moving to go-cloudformation

--- ansible/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-virtualenv wheel
-source wheel/bin/activate
-pip install -r requirements.txt
-#ansible-galaxy install -r Ansiblefile -p roles
--- ansible/ping-all.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ENVIRONMENT=${1:-ci}
-ansible all -i environments/$ENVIRONMENT/inventory --private-key=environments/$ENVIRONMENT/.keys/dcos-bootstrap.pem -m ping
